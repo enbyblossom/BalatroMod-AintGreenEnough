e23ce4cf22ed19d275a3707d95099b29bd27ac4c
enbyblossom <enbyblossomgithub@gmail.com>
2026-02-19 01:09:26 +0000

    Update AintGreenEnough.lua

--- AintGreenEnough/AintGreenEnough.lua
+++ AintGreenEnough/AintGreenEnough.lua
@@ -1,3 +1,3 @@
 sendDebugMessage("Launching Negate Texture Pack!", "NegateTexturePack")
 
-SMODS.Atlas{key = "Joker", path = "Jokers-negate.png", px = 71, py = 95, prefix_config = { key = false } }
+SMODS.Atlas{key = "Joker", path = "Jokers-Pos.png", px = 71, py = 95, prefix_config = { key = false } }
